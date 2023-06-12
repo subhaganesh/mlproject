dd1c6da14db350306cdf42c95f2a1acc7f09a631
subhaganesh <subhaganesh5@outlook.com>
2023-06-12 10:03:57 +0530

    logging&exception

--- src/exception.py
+++ src/exception.py
@@ -1,23 +1,27 @@
 import sys
-import logging
+import logger
 
-
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number[{1}] error message [{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error))
-
+def get_error_message(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in the Python script: {file_name}, Line: {line_number}, Error: {str(error)}"
     return error_message
 
-    
-
-class customException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(self,error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(str(error))
+        self.error_message = get_error_message(error, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except ZeroDivisionError as e:
+        logger.setup_logging()
+        logger.logging.error("Divide by zero error")
+        raise CustomException(e, sys)
 
 
