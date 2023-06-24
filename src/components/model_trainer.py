8d65418c37b28cf2d2079fea76b93720d4e69c2a
subhaganesh <subhaganesh5@outlook.com>
2023-06-24 20:21:56 +0530

    'predictpipeline'

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -98,6 +98,7 @@ class modeltrainer():
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+            
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
